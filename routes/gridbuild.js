0d3e8750f81c7bb109c9158abeb4c32821fd35d7
SaiGeethikagarikapati <108358561+SaiGeethikagarikapati@users.noreply.github.com>
2022-11-02 18:38:01 -0500

    for part 6

---
+++ routes/gridbuild.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    let query = req.query
+    console.log(`rows ${query.rows}`)
+    res.render('gridbuild', { title: 'Grid Build', query: query });
+});
+
+module.exports = router;
